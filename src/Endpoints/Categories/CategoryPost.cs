b9684803fad60be05fabf050ebf756179ffbd894
Matheus Garcia <matheusfrg@gmail.com>
2022-10-20 17:17:58 -0300

    Inclusão dos métodos e validações

---
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,25 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+    public static IResult Action (CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        var category = new Category(categoryRequest.Name, "Matheus Garcia", "Matheus Garcia");
+
+        if (!category.IsValid)
+        {            
+            return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
+        }
+
+        context.Categories.Add(category);
+        context.SaveChanges();
+
+        return Results.Created($"/categories/{category.Id}", category.Id);
+    }
+}
