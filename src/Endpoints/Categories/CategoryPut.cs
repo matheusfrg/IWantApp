215419a238d559b7d9863c215760a5eb7d2721fa
Matheus Garcia <matheusfrg@gmail.com>
2022-10-25 16:04:57 -0300

    Autenticação por JWT

--- src/Endpoints/Categories/CategoryPut.cs
+++ src/Endpoints/Categories/CategoryPut.cs
@@ -1,5 +1,7 @@
 ﻿using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Categories;
 
@@ -8,14 +10,17 @@ public class CategoryPut
     public static string Template => "/categories/{id:guid}";
     public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
     public static Delegate Handle => Action;
-    public static IResult Action ([FromRoute] Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
+    
+    [Authorize(Policy = "EmployeePolicy")]
+    public static IResult Action ([FromRoute] Guid id, HttpContext http, CategoryRequest categoryRequest, ApplicationDbContext context)
     {
+        var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
 
         if (category == null)
             return Results.NotFound();
 
-        category.EditInfo(categoryRequest.Name, categoryRequest.Active);
+        category.EditInfo(categoryRequest.Name, categoryRequest.Active, userId);
 
         if (!category.IsValid)
             return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
