cc9bee4666cc2aece7db5970c0ffb4fbf9d732e6
Matheus Garcia <matheusfrg@gmail.com>
2022-10-26 08:51:33 -0300

    Auditorias

--- src/Endpoints/Employees/EmployeePost.cs
+++ src/Endpoints/Employees/EmployeePost.cs
@@ -8,11 +8,12 @@ public class EmployeePost
     public static string Template => "/employees";
     public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
     public static Delegate Handle => Action;
-    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    public static IResult Action(EmployeeRequest employeeRequest, HttpContext http, UserManager<IdentityUser> userManager)
     {
-        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+        var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+        var newUser = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
 
-        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+        var result = userManager.CreateAsync(newUser, employeeRequest.Password).Result;
 
         if (!result.Succeeded)
             return Results.ValidationProblem(result.Errors.ConvertToProblemDetails());
@@ -20,14 +21,15 @@ public class EmployeePost
         var userClaims = new List<Claim>
         {
             new Claim("EmployeeCode", employeeRequest.EmployeeCode),
-            new Claim("Name", employeeRequest.Name)
+            new Claim("Name", employeeRequest.Name),
+            new Claim("CreatedBy", userId),
         };
 
-        var claimResult = userManager.AddClaimsAsync(user, userClaims).Result;
+        var claimResult = userManager.AddClaimsAsync(newUser, userClaims).Result;
 
         if (!claimResult.Succeeded)
             return Results.BadRequest(claimResult.Errors.First());
 
-        return Results.Created($"/employees/{user.Id}", user.Id);
+        return Results.Created($"/employees/{newUser.Id}", newUser.Id);
     }
 }
