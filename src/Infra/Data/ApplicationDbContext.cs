acba49a78088adbe4bc6800711f1a48e1b2f3836
Matheus Garcia <matheusfrg@gmail.com>
2022-11-03 10:38:43 -0300

    Inclusão de novas features

--- src/Infra/Data/ApplicationDbContext.cs
+++ src/Infra/Data/ApplicationDbContext.cs
@@ -5,7 +5,7 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)   { }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -17,6 +17,8 @@ public class ApplicationDbContext : IdentityDbContext<IdentityUser>
             .Property(p => p.Name).IsRequired();
         builder.Entity<Product>()
             .Property(p => p.Description).HasMaxLength(255);
+        builder.Entity<Product>()
+            .Property(p => p.Price).HasColumnType("decimal(10,2)").IsRequired();
 
         builder.Entity<Category>()
             .Property(p => p.Name).IsRequired();
--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ using IWantApp.Endpoints.Products;
 using Microsoft.AspNetCore.Diagnostics;
 using Serilog;
 using Serilog.Sinks.MSSqlServer;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseSerilog((context, configuration) =>
@@ -94,6 +95,8 @@ app.Map("/error", (HttpContext http) =>
     {
         if (error is SqlException)
             return Results.Problem(title: "Database out", statusCode: 500);
+        else if (error is BadHttpRequestException)
+            return Results.Problem(title: "Error to convert data to other type. See all the information sent", statusCode: 500);
     }
 
     return Results.Problem(title: "An error ocurred", statusCode: 500);
